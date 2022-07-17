72dccf97f86f4532e12f844c77f4f84299a10813
ada1012 <alber.diaz.toledo@gmail.com>
2022-07-17 21:53:32 +0200

    Add permissions and edit gps_bloc

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -10,5 +11,19 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
       isGpsEnabled: event.isGpsEnabled,
       isPermissionGranted: event.isGpsPermissionGranted
     )));
+
+    _init();
+  }
+
+  Future<void> _init() async {
+    _checkGpsStatus();
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+    Geolocator.getServiceStatusStream().listen((event) {
+
+    });
+    return isEnable;
   }
 }
